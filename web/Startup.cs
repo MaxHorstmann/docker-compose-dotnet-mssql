cdb730da6689304a36217e49a637909a3d6502c2
Max Horstmann <mhorstmann@stackoverflow.com>
2018-12-17 21:47:31 -0500

    default to Customers

--- web/Startup.cs
+++ web/Startup.cs
@@ -42,7 +42,7 @@ namespace test
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Customers}/{action=Index}/{id?}");
             });
         }
     }
