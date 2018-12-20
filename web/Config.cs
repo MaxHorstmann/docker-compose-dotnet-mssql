3ce81c952b113f2b938e95efaa8874ccea0c3622
Max Horstmann <mhorstmann@stackoverflow.com>
2018-12-19 19:37:13 -0500

    make idempotent

--- web/Config.cs
+++ web/Config.cs
@@ -25,15 +25,18 @@ namespace test
             using (SqlConnection connection = new SqlConnection(Config.ConnectionString))   
             {
                 connection.Execute(@"
-                    create table Customers
-                    (
-                      id int not null identity(1,1) constraint pk_customers primary key,
-                      name nvarchar(100) not null
-                    )
+                    if not exists(select 1 from sys.tables where name='Customers')
+                    BEGIN
+                        create table Customers
+                        (
+                          id int not null identity(1,1) constraint pk_customers primary key,
+                          name nvarchar(100) not null
+                        )
 
-                    insert into customers (name) values ('John')
-                    insert into customers (name) values ('Ella')
-                    insert into customers (name) values ('Peter')
+                        insert into customers (name) values ('John')
+                        insert into customers (name) values ('Ella')
+                        insert into customers (name) values ('Peter')
+                    END
                     ");
             }
         }
